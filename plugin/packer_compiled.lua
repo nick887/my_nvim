d4ec2f33abd7a0250f139bcc9cddc5fbed382e2f
nick <nick.xiao@momenta.ai>
2022-07-30 22:35:59 +0800

    remove gentags

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -134,11 +134,6 @@ _G.packer_plugins = {
     path = "/Users/txiao/.local/share/nvim/site/pack/packer/start/vim-go",
     url = "https://github.com/fatih/vim-go"
   },
-  ["vim-gutentags"] = {
-    loaded = true,
-    path = "/Users/txiao/.local/share/nvim/site/pack/packer/start/vim-gutentags",
-    url = "https://github.com/ludovicchabant/vim-gutentags"
-  },
   ["vim-nerdtree-syntax-highlight"] = {
     loaded = true,
     path = "/Users/txiao/.local/share/nvim/site/pack/packer/start/vim-nerdtree-syntax-highlight",
